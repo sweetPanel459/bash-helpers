0de97fe99857dbfa4a8a2a055138f86b22f6ad68
otpJs <sssantiago459@gmail.com>
2024-11-01 21:58:30 -0500

    feat: creating script for tasks

---
+++ scripts/todo.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# TODO:
+#  - crear un array o objeto que contenga todos las opciones del script
+#  - crear una funcion que apenas se ejecute el script se llame y evalue si tiene alguna entrada estandar, confirmando que hacer
+#  - si no existe un md crearlo y si no pasarlo por alto
+#  - aprender awk a profundida o hasta que me permita manipular archivos de texto
+#  - crear diversas funcionens para la operacion curd del md
+#     - la funcion view listara todos los id y nombres de las listas y si pongo un id se vera esa lista
+#     - la funcion modofy obtendre todo el md y con cada id  de la lista y si incerta nama mas un id modificara el titulo, si son id id-row, se modificar una fila
+#     - la funcion add solo agregara una lista y si es es con id agregara una fila o varias en una lista existente
+#     - la funcion delete borrara una lista o fila de una lista
